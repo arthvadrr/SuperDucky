14ffee25edc46f5064cb7ee8c71431eef6d28b95
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2025-04-26 23:08:57 -0500

    feat: Color utility

---
+++ vue_overlay/src/util/getHueRotateAmount.ts
@@ -0,0 +1,35 @@
+export function getHueFromHex(hex: string): number {
+  const hexString: string = hex.slice(1);
+  const value: number = Number('0x' + hexString);
+  const r: number = value >> 16;
+  const g: number = (value >> 8) & 0xff;
+  const b: number = value & 0xff;
+  const max: number = Math.max(r, g, b);
+  const min: number = Math.min(r, g, b);
+  const delta: number = max - min;
+
+  let hue: number = 0;
+
+  if (delta !== 0) {
+    if (r === max) {
+      hue = (60 * (g - b)) / delta;
+    } else if (g === max) {
+      hue = (60 * (b - r)) / delta + 120;
+    } else {
+      hue = (60 * (r - g)) / delta + 240;
+    }
+    hue = (hue + 360) % 360;
+  }
+
+  return hue;
+}
+
+export function getHueRotateAmount(
+  targetHex: string,
+  baseHex: string = '#FFDB50',
+): number {
+  const hue: number = getHueFromHex(targetHex);
+  const baseHue: number = getHueFromHex(baseHex);
+
+  return (hue - baseHue + 360) % 360;
+}
